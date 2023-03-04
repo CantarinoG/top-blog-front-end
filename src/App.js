437edaba79c1d192bfc746c11d612f559e291c37
CantarinoG <guilhermecantarino1709@gmail.com>
2023-03-03 22:18:02 -0300

    Change base url to the deployed api

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   const [loggedIn, setLoggedIn] = useState(false);
   const [token, setToken] =  useState(undefined);
   const [path, setPath] = useState('/posts');
-  const [baseURL, setBaseURL] = useState('http://localhost:2000');
+  const [baseURL, setBaseURL] = useState('https://top-blog-api.cantarinog.repl.co');
   const [currentPost, setCurrentPost] = useState('');
 
   useEffect(() => { //ComponentDidMount
